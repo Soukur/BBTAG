a28909952297f49b3b04eaca8498b2cb764e029c
soma <0924soma@gmail.com>
2023-05-13 01:47:12 +0900

    stateを使う

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
+import * as React from 'react';
 import { Button } from './components/button';
 
 const App = () => {
+  const [teamPower, setTeamPower] = React.useState(0);
   return (
     <>
       <h1 className='text-emerald-400 text-3xl'>BBTAG最高のゲーム</h1>
+      <div>{teamPower}</div>
 
       <div className='max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
         <h5 className='mb-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>セトヒルダ</h5>
 
         <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>214PA</p>
-        <Button>おわっとるが</Button>
+
         <Button
           onClick={() => {
-            alert('じゃない');
+            setTeamPower((num) => num + 2);
           }}
         >
-          可愛いだけ
+          +
         </Button>
-        <Button>punpun</Button>
-        <Button>バイバイ！</Button>
+        <Button
+          onClick={() => {
+            setTeamPower((num) => num - 2);
+          }}
+        >
+          -
+        </Button>
       </div>
     </>
   );
