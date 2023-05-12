877778edf890ece95115ea173c689267aa205f6c
soma <0924soma@gmail.com>
2023-05-13 01:08:52 +0900

    セトヒルダ終わっとる画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 const App = () => {
   return (
     <>
-      <h1 className='text-blue-900'>BBTAG最高のゲーム</h1>
+      <h1 className='text-emerald-400 text-3xl'>BBTAG最高のゲーム</h1>
+
+      <div className='max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+        <h5 className='mb-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>セトヒルダ</h5>
+
+        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>214PA</p>
+        <button className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+          終わっとる
+        </button>
+      </div>
     </>
   );
 };
