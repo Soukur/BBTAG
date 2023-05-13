abc88b46d7b303810f0475ca460a5efe11f7740a
soma <0924soma@gmail.com>
2023-05-13 20:47:18 +0900

    レスポンシブと配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-1': 'url(/Users/sou/BBTAG/src/images/bg.jpg)',
+      },
+    },
   },
   plugins: [],
 };
