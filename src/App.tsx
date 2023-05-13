6bb4ce46c8641d290bf4a6b1f8419abb7a50a655
soma <0924soma@gmail.com>
2023-05-14 07:00:01 +0900

    背景画像きた

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,50 @@
 import * as React from 'react';
 import { Button } from './components/button';
+import bgImage from './images/bg.jpg';
 
 const App = () => {
   const [teamPower, setTeamPower] = React.useState(0);
+
+  const backgroundImageStyle = {
+    backgroundImage: `url("${bgImage}")`,
+    backgroundSize: 'cover',
+  };
+
   return (
-    <body className='bg-slate-500 text-2xl'>
-      <h1 className='text-3xl flex justify-center items-center bg-white h-32  text-emerald-400    '>
-        BBTAG TEAM RATING
-      </h1>
+    <div className='App'>
+      <div className='bg-slate-500 border-2 border-blue-700' style={backgroundImageStyle}>
+        <h1 className=' border-2 border-red-700 text-3xl flex justify-center items-center font-bold text-black h-32 '>
+          BBTAG TEAM RATING
+        </h1>
 
-      <section className='flex'>
-        <div className='p-2 w-1/2 bg-green-500'>LEFT</div>
+        <div className='flex'>
+          <div className='p-2 w-1/2 border-2 border-green-500'>LEFT</div>
 
-        <div className='p-2 w-1/2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-          <h5 className='mb-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>セトヒルダ</h5>
-
-          <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>214PA</p>
-
-          <Button
-            onClick={() => {
-              setTeamPower((num) => num + 2);
-            }}
-          >
-            +
-          </Button>
-          <Button
-            onClick={() => {
-              setTeamPower((num) => num - 2);
-            }}
-          >
-            -
-          </Button>
+          <div className='p-2 w-1/2  border-2 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+            <div className='grid grid-rows-6 gap-2'>
+              <div className='mb-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                TEAMPOWER
+                <Button
+                  onClick={() => {
+                    setTeamPower((num) => num + 1);
+                  }}
+                >
+                  +
+                </Button>
+                <Button
+                  onClick={() => {
+                    setTeamPower((num) => num - 1);
+                  }}
+                >
+                  -
+                </Button>
+                <div>{teamPower}</div>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-
-      <div>{teamPower}</div>
-    </body>
+      </div>
+    </div>
   );
 };
 
