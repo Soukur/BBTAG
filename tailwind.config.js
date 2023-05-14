e9a4a670e059047456d9807978ba725dbe491246
soma <0924soma@gmail.com>
2023-05-14 09:31:58 +0900

    materialtailwind導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -8,4 +10,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
