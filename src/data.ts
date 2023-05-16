642f1b30d5be103bb9a2f7bb91ceb4b1e58d5532
soma <0924soma@gmail.com>
2023-05-17 03:42:10 +0900

    位置調節と背景

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
+import adachiImage from './images/character/adachi.png';
 import sethImage from './images/character/seth.png';
 import hildaImage from './images/character/hilda.png';
-import adachiImage from './images/character/adachi.png';
 import yuImage from './images/character/yu.png';
 
 export const images = {
